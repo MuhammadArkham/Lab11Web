47c00ff82ea4ef051e35e60e2cc9658ba9c58ef5
MuhammadArkham <arkhamqwe123@gmail.com>
2025-12-24 11:26:06 +0700

    Delete lab11_php_oop directory

--- lab11_php_oop/template/footer.php
+++
@@ -1,5 +0,0 @@
-</div>
-    </div>
-    <footer><p>&copy; 2025 Universitas Pelita Bangsa</p></footer>
-</body>
-</html>
--- lab11_php_oop/template/sidebar.php
+++
@@ -1,8 +0,0 @@
-<div class="sidebar">
-    <h3>Navigasi</h3>
-    <ul>
-        <li><a href="/lab11_php_oop/index.php/artikel/index">Home</a></li>
-        <li><a href="/lab11_php_oop/index.php/artikel/index">Data Artikel</a></li>
-        <li><a href="/lab11_php_oop/index.php/artikel/tambah">Tambah Artikel</a></li>
-    </ul>
-</div>
